3e1de635a2d898327918ad538a32458188a3194a
Martin Benonisen <mbenonisen@gmail.com>
2013-05-04 15:36:26 +0200

    Updated interpreter.py: added load file function

--- interpreter.py
+++ interpreter.py
@@ -1,4 +1,5 @@
 import sys, os
+from time import sleep
 screenbuffer = ""
 pointer = 0
 def inc_ptr():
@@ -19,7 +20,7 @@ def wr():
 def rd():
     global pointer
     MEM[pointer] = ord(sys.stdin.read(1))
-    
+
 def runthis(this):
     for count in range(MEM[pointer]):
         for thing in this:
@@ -27,6 +28,29 @@ def runthis(this):
                 INTERP.get(thing)()
             except:
                 pass
+def load_program(filename):
+    whilebuff = ""
+    loop = False
+    try:
+        with open(filename) as f:
+            for j in f:
+                for i in j:
+                    if i is '[' or loop:
+                        loop = True
+                        if i == ']':
+                            runthis(whilebuff[1:])
+                            loop = False
+                        whilebuff += i
+                    else:
+
+                        try:
+                            INTERP.get(i)()
+                        except:
+                            pass
+    except:
+        print "File not found"
+        sleep(0.5)
+
 INTERP = {'+': inc_deref_ptr, '>': inc_ptr,'<': dec_ptr, '-':
         dec_deref_ptr, '.': wr, ',': rd}
 
@@ -39,6 +63,9 @@ while 1:
     print screenbuffer
     print ">>> ",
     j = raw_input()
+    if "load" in j:
+        load_program(j[5:])
+        continue
     for i in j:
         if i is '[' or loop:
             loop = True
