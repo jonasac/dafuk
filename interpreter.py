4563eea712834074bd05d9fb7695e129240bbe0d
Martin Benonisen <mbenonisen@gmail.com>
2013-05-04 03:23:17 +0200

    Removed a couple of bugs

--- interpreter.py
+++ interpreter.py
@@ -1,5 +1,5 @@
-import sys
-
+import sys, os
+screenbuffer = ""
 pointer = 0
 def inc_ptr():
     global pointer
@@ -14,18 +14,21 @@ def dec_deref_ptr():
     global pointer
     MEM[pointer] -= 1
 def wr():
-    global pointer
-    sys.stdout.write(chr(MEM[pointer]))
+    global pointer, screenbuffer
+    screenbuffer += chr(MEM[pointer])
+#    sys.stdout.write(chr(MEM[pointer]))
 def rd():
     global pointer
-    MEM[pointer] = sys.stdin.read(1)
+    MEM[pointer] = ord(sys.stdin.read(1))
 def junk():
     pass
 def runthis(this):
     for count in range(MEM[pointer]):
         for thing in this:
-            INTERP.get(thing)()
-
+            try:
+                INTERP.get(thing)()
+            except:
+                pass
 INTERP = {'+': inc_deref_ptr, '>': inc_ptr,'<': dec_ptr, '-':
         dec_deref_ptr, '.': wr, ',': rd, '\n': junk, ' ': junk}
 
@@ -34,6 +37,9 @@ MEM = [0 for i in range(30000)]
 loop = False
 whilebuff = ""
 while 1:
+    os.system("clear")
+    print screenbuffer
+    print ">>> ",
     j = raw_input()
     for i in j:
         if i is '[' or loop:
@@ -43,4 +49,7 @@ while 1:
                 loop = False
             whilebuff += i
         else:
-            INTERP.get(i)()
+            try:
+                INTERP.get(i)()
+            except:
+                pass
